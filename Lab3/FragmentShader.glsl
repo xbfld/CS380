7529fb5f2cbbed5180839ec75e2507a24069c9e5
Joon Kim <cehigi@gmail.com>
2016-05-04 19:59:56 +0900

    Phong shading

--- Lab3/FragmentShader.glsl
+++ Lab3/FragmentShader.glsl
@@ -15,7 +15,15 @@ void main(){
 	//TODO: Assign fragmentColor as a final fragment color
 	// color = fragmentColor;
 	//TODO:Assign fragmentNormal as a final fragment color
-	vec3 normal = normalize(fragmentNormal);
-    color = normal;
-	//TODO: Phong reflection model	
+	// vec3 normal = normalize(fragmentNormal);
+    // color = normal;
+	//TODO: Phong reflection model	l
+    vec3 tolight = normalize(uLight - fragmentPosition);
+    vec3 toV = -normalize(vec3(fragmentPosition));
+    vec3 h = normalize(toV + tolight);
+    vec3 normal = normalize(fragmentNormal);
+    float specular = pow(max(0.0, dot(h, normal)), 64.0);
+    float diffuse = max(0.0, dot(normal, tolight));
+    vec3 intensity = fragmentColor *diffuse + vec3(0.6, 0.6, 0.6)*specular;
+    color = pow(intensity, vec3(1.0 / 2.2)); // Apply gamma correction 
 }
